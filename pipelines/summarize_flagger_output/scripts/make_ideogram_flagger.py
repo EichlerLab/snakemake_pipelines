e6e56b9d7ac8756d2f92b9a2ba725cf4052c2912
Julie Wertz <jwertz1@uw.edu>
2023-10-05 14:59:39 -0700

    Run black

--- pipelines/summarize_flagger_output/scripts/make_ideogram_flagger.py
+++ pipelines/summarize_flagger_output/scripts/make_ideogram_flagger.py
@@ -45,8 +45,8 @@ def ideo_cb(df, chrom, ax, fig):
     # SVPop removes all columns except #CHROM, POS, END,
     # and label_col from the df and divides bins in half, use original df
 
-    mpl.rcParams.update({'font.size':8})
-    asms = sorted(df['ASM'].unique(), reverse=True)
+    mpl.rcParams.update({"font.size": 8})
+    asms = sorted(df["ASM"].unique(), reverse=True)
 
     max_bar_height = len(samples) * len(asms)
     df = all_samples_df
@@ -81,13 +81,15 @@ def ideo_cb(df, chrom, ax, fig):
     # Show legend
     if len(asms) > 1:
         h, l = ax.get_legend_handles_labels()
-        legend_d.update({l[i]:h[i] for i in range(len(h))})
+        legend_d.update({l[i]: h[i] for i in range(len(h))})
 
-        if chrom == 'chrY':
+        if chrom == "chrY":
             asms_for_legend = asms[::-1]
             ax.legend(
-                labels=asms_for_legend, handles=[legend_d[x] for x in asms_for_legend],
-                loc='center left', bbox_to_anchor=(-1.5, 0.35)
+                labels=asms_for_legend,
+                handles=[legend_d[x] for x in asms_for_legend],
+                loc="center left",
+                bbox_to_anchor=(-1.5, 0.35),
             )
 
 
@@ -101,7 +103,7 @@ for infile in infiles:
     # print(f'Reading in {infile}')
     sample = os.path.basename(infile).split(".")[0]  # Since PAFs are named {sample}.paf
     print(f"Sample {sample}")
-   
+
     df = pd.read_csv(
         infile,
         sep="\t",
@@ -118,9 +120,7 @@ for infile in infiles:
         if not asm:
             continue
         asm_bed = BedTool.from_dataframe(df.loc[df["ASM"] == asm])
-        asm_df = bins_bed.intersect(
-            asm_bed, wa=True, u=True
-        ).to_dataframe()
+        asm_df = bins_bed.intersect(asm_bed, wa=True, u=True).to_dataframe()
 
         if asm_df.empty:
             continue
--- pipelines/summarize_flagger_output/scripts/make_windows_bed.py
+++ pipelines/summarize_flagger_output/scripts/make_windows_bed.py
@@ -9,16 +9,19 @@ out_fname = snakemake.output.bed
 
 fai_filename = snakemake.input.fai
 
-with open(out_fname, 'w') as out_f:
+with open(out_fname, "w") as out_f:
     with open(fai_filename) as f:
-        r = csv.DictReader(f, delimiter='\t', fieldnames=['#CHROM', 'LENGTH'])
+        r = csv.DictReader(f, delimiter="\t", fieldnames=["#CHROM", "LENGTH"])
         for row in r:
-            out_intervals = list(
-                range(0, int(row['LENGTH']) + BIN_SIZE, BIN_SIZE)
-            )
+            out_intervals = list(range(0, int(row["LENGTH"]) + BIN_SIZE, BIN_SIZE))
             for i in range(len(out_intervals) - 1):
-                out_f.write('\t'.join([
-                    row['#CHROM'],
-                    str(out_intervals[i]),
-                    str(out_intervals[i + 1])
-                ])+ '\n')
+                out_f.write(
+                    "\t".join(
+                        [
+                            row["#CHROM"],
+                            str(out_intervals[i]),
+                            str(out_intervals[i + 1]),
+                        ]
+                    )
+                    + "\n"
+                )
